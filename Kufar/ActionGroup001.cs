0854c5ac8493b554e7a4aeb00d9aa70dee8489c5
srgo2 <srgo2@DESKTOP-F2FP0OU>
2021-02-25 05:12:51 +0400

    Добавьте файлы проекта.

---
+++ Kufar/ActionGroup001.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Drawing;
+using System.Resources;
+using System.ComponentModel;
+using System.Collections.Generic;
+using ZennoLab.CommandCenter;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using ZennoLab.InterfacesLibrary.ProjectModel.Enums;
+using ZennoLab.Emulation;
+using Global.ZennoExtensions;
+using ZennoLab.CommandCenter.TouchEvents;
+using ZennoLab.CommandCenter.FullEmulation;
+using ZennoLab.InterfacesLibrary.Enums;
+
+namespace Kufar
+{
+	/// <summary>
+	/// Класс выполнения группы действий
+	/// </summary>
+	internal class ActionGroup001
+	{
+		/// <summary>
+		/// Метод выполнения группы действий
+		/// </summary>
+		/// <param name="instance">Объект инстанса выделеный для данного скрипта</param>
+		/// <param name="project">Объект проекта выделеный для данного скрипта</param>
+		/// <returns>Код выполнения группы действий</returns>
+		public static int Execute(Instance instance, IZennoPosterProjectModel project)
+		{
+			instance.ClearCookie();
+
+			Tab tab = instance.ActiveTab;
+			if ((tab.IsVoid) || (tab.IsNull)) return -1;
+			if (tab.IsBusy) tab.WaitDownloading();
+			tab.Navigate("https://www.kufar.by/listings", "");
+			if (tab.IsBusy) tab.WaitDownloading();
+
+
+			return 0;
+		}
+	}
+}
---
+++ Kufar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Resources;
+using System.Text;
+using ZennoLab.CommandCenter;
+using ZennoLab.Emulation;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+using ZennoLab.InterfacesLibrary.ProjectModel.Enums;
+
+namespace Kufar
+{
+    /// <summary>
+    /// Класс для запуска выполнения скрипта
+    /// </summary>
+    public class Program : IZennoExternalCode
+    {
+        /// <summary>
+        /// Метод для запуска выполнения скрипта
+        /// </summary>
+        /// <param name="instance">Объект инстанса выделеный для данного скрипта</param>
+        /// <param name="project">Объект проекта выделеный для данного скрипта</param>
+        /// <returns>Код выполнения скрипта</returns>		
+        public int Execute(Instance instance, IZennoPosterProjectModel project)
+        {
+            int executionResult = 0;
+
+			// Выполнить группу действий ActionGroup001
+			executionResult = ActionGroup001.Execute(instance, project);
+			if (executionResult != 0) return executionResult;
+
+            return executionResult;
+        }
+    }
+}
